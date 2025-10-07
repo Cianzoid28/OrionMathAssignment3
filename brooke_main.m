1ba871b54f5edd7261c267cd4b6d0c21c32af9ab
Brooke Wager <bwager@olin.edu>
2025-10-06 21:14:34 -0400

    started forward euler step

---
+++ brooke_main.m
@@ -0,0 +1,47 @@
+% forward_euler_step
+t = 0;
+X0 = 0;
+h = 0.01;
+
+forward_euler_step(@rate_func01, t, X0, h)
+solution01(t)
+
+forward_euler_step(@rate_func02, t, X0, h)
+solution02(t)
+
+% forward_euler_step_integration
+t_span = [0, 10];
+X0 = 1;
+
+[t_list, X_list, h_avg, num_evals] = forward_euler_fixed_step_integration(@rate_func01, t_span, X0, h);
+solution_X_list = zeros(length(X0), length(t_list));
+for i = 1:length(t_list)
+    solution_X_list(i) = solution01(t_list(i));
+end
+
+figure();
+hold on;
+
+plot(t_list, X_list);
+plot(t_list, solution_X_list)
+xlabel('Time');
+ylabel('Solution');
+title('Forward Euler Integration Results');
+grid on;
+
+% defined functions
+function dXdt = rate_func01(t,X)
+    dXdt = -5*X + 5*cos(t) - sin(t);
+end
+
+function X = solution01(t)
+    X = cos(t);
+end
+
+function dXdt = rate_func02(t,X)
+    dXdt = [0,-1;1,0]*X;
+end
+
+function X = solution02(t)
+    X = [cos(t);sin(t)];
+end
